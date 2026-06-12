ff12046a9ce89c4eb983b20a05c8ef0b9363634c
taic101400-png <taic101400@gmail.com>
2026-06-12 14:24:30 +0900

    公開用設定ファイルを追加

---
+++ config.js
@@ -0,0 +1,5 @@
+window.CLIMB_SYNC_CONFIG = {
+  url: "https://zfsbaqzuzncajsofwnja.supabase.co",
+  anonKey: "sb_publishable_q7jPguVizsQjCvH_69EE9w_0q4Tp9Qr",
+  inviteKey: "Mb3WkoXoeYeGBoFrmJRzJ0kIutO1UNb1"
+};
